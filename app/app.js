1594ea250025c778edbe6a5614f037799019e84a
Bernardo Peña <bernardo.pena.ramos@gmail.com>
2015-09-15 01:12:58 -0430

    Se agregó la directiva ptSelect

--- app/app.js
+++ app/app.js
@@ -4,12 +4,20 @@
     // Declare app level module which depends on views, and components
     angular
         .module('myApp', [
-            'directives',
+            'components',
             'ngMaterial',
             'ngMessages'
         ])
         .controller('MyController', ['$scope', function($scope) {
-            $scope.valor = 30;
+            var vm = this;
+            vm.valor = 30;
+
+            vm.userState = '';
+            vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+            'WY').split(' ').map(function (state) { return { abbrev: state }; });
+
+            console.log(vm.states);
         }])
         .config(function($mdThemingProvider) {
             $mdThemingProvider.theme('docs-dark', 'default')
--- app/directives/input/input.directive.js
+++ app/directives/input/input.directive.js
@@ -2,8 +2,10 @@
     'use strict';
 
     angular
-        .module('directives', [])
-        .directive('ptInput', ptInput);
+        .module('components', [])
+        .directive('ptInput', ptInput)
+        .directive('ptSelect', ptSelect)
+    ;
 
     function ptInput() {
         var directive = {
@@ -13,9 +15,29 @@
                 title: '@'
             },
             template: "<md-input-container>\
-                            <label>{{title}}</label>\
-                            <input data-ng-model='ngModel' \
-                        </md-input-container>"
+                           <label>{{title}}</label>\
+                           <input data-ng-model='ngModel' \
+                       </md-input-container>"
+        };
+
+        return directive;
+    }
+
+    function ptSelect() {
+        var directive = {
+            replace: true,
+            scope: {
+                ngModel: '=',
+                title: '@',
+                valueN: '=',
+                values: '='
+            },
+            template: "<md-input-container>\
+                           <label>{{title}}</label>\
+                           <md-select data-ng-model='ngModel'>\
+                               <md-option data-ng-repeat='state in values' value='{{state.abbrev}}'>{{state.abbrev}}</md-option>\
+                           </md-select>\
+                       </md-input-container>"
         };
 
         return directive;
